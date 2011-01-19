bf2fbbe243818961756eb86479a555438e0b93a5
David Yip <yipdw@member.fsf.org>
2011-01-19 02:46:38 -0600

    Rearranged $LOAD_PATH.unshift statement.
    This doesn't affect the testcase run results, as far as I can tell.

--- foo.rb
+++ foo.rb
@@ -1,10 +1,10 @@
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)) + '/./foo')
+
 require 'pp'
 
 module Foo
   autoload :Bar, 'bar'
 end
-
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)) + '/./foo')
 
 pp $LOAD_PATH
 pp Foo::Bar
